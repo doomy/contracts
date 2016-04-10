51226900a490aa447783bab526a1dda9cce728d1
doomy <vladimir.bartek@gmail.com>
2016-04-10 14:02:56 +0200

    Implicit dataset

---
+++ dataset.sql
@@ -0,0 +1,6 @@
+INSERT INTO t_contract_type (contract_type) VALUES
+    ('AÜG'), ('FL'), ('FMA'), ('local'), ('WV');
+    
+INSERT INTO t_contract_status (contract_status) VALUES
+    ('new'), ('internal'), ('sent'), ('signed'), ('storno'); 
+    
