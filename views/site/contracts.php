c9690b6be13719eac7be9606d7ea4ada4087c08e
doomy <vladimir.bartek@gmail.com>
2016-04-11 20:37:00 +0200

    Add title to contracts page

--- views/site/contracts.php
+++ views/site/contracts.php
@@ -1,6 +1,12 @@
-<?php use yii\helpers\Html; ?>
+<?php use yii\helpers\Html;
+
+$this->title = "Contracts";
+
+?>
 
 <div>
+    <h1><?= Html::encode($this->title) ?></h1>
+
     <table>
     <?php
         foreach ($contracts as $contract) { ?>
