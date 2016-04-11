024afde885083d603910ee2be3aeaa960fa1fc30
doomy <vladimir.bartek@gmail.com>
2016-04-11 22:15:19 +0200

    Specify contract columns in the grid view

--- views/site/contracts.php
+++ views/site/contracts.php
@@ -9,6 +9,17 @@ echo "<h1>{$this->title}</h1>";
 
 echo GridView::widget([
     'dataProvider' => $dataProvider,
+    'columns' => [
+        'id',
+        'contract_type_id',
+        'contract_status_id',
+        'company_name',
+        'contract_subject',
+        'contract_value',
+        'created_at',
+        'active_from',
+        'active_to'
+    ]
 ]);
 
 
